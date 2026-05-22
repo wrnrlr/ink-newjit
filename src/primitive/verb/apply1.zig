8c90c2a2495ae05988291dd7614c23213e32dbd1
wrnrlr <werner@alman.ax>
2026-05-23 00:41:10 +0200

    apply1 file

---
+++ src/primitive/verb/apply1.zig
@@ -0,0 +1,15 @@
+const std = @import("std");
+const value = @import("../../noun/value.zig");
+const calc = @import("./calc.zig");
+const util = @import("../../util.zig");
+const VM = @import("../../runtime/vm.zig").VM;
+const V = value.V;
+const Dict = value.Dict;
+const Alloc = std.mem.Allocator;
+
+const pick = @import("pick.zig");
+
+pub const Apply1 = struct {
+  pub const op = .@"@";
+
+};
