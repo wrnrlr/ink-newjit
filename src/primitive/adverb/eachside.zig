94e5a793de125dcb691913393956cc2552f40ded
wrnrlr <werner@alman.ax>
2026-05-23 22:03:46 +0200

    Refactor eachside

--- src/primitive/adverb/eachside.zig
+++ src/primitive/adverb/eachside.zig
@@ -17,3 +17,15 @@ pub fn eachright(vm: *VM, base: V, x: V, y: V, f: util.ApplyFn) V {
   }
   return .{ .L = res };
 }
+
+pub fn eachleft(vm: *VM, base: V, x: V, y: V, f: util.ApplyFn) V {
+  const n = x.len();
+  var res = N(V).init(vm.alloc, n) catch return V{ .err = .memory };
+  for (0..n) |i| {
+    const item = x.at(i);
+    defer item.deinit(vm.alloc);
+    const args = [_]V{ item, y };
+    res.slice()[i] = f(vm, base, &args);
+  }
+  return .{ .L = res };
+}
